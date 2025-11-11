16d878e1b89df87d529278a687f46d0b99ef90b6
dhytalu <nurdita07@gmail.com>
2025-11-11 15:13:44 +0700

    style(templates): improve button layout in portfolio template

--- templates/single-portofolio.php
+++ templates/single-portofolio.php
@@ -21,20 +21,22 @@ get_header();
         <div class="col-md-8">
           <div class="entry-header">
             <h1 class="entry-title"><?php the_title(); ?></h1>
-            <?php
-              $live = get_post_meta( get_the_ID(), '_wssp_url_live_preview', true );
-              $wa = function_exists('wssp_get_whatsapp_order_url') ? wssp_get_whatsapp_order_url( get_the_ID() ) : '';
-              if ( $live ) :
-            ?>
-              <p class="mt-2 mb-3">
-                <a class="btn btn-primary" href="<?php echo esc_url( $live ); ?>" target="_blank" rel="noopener"><i class="fa fa-eye pe-2"></i>Live Preview</a>
-              </p>
-            <?php endif; ?>
-            <?php if ( ! empty( $wa ) ) : ?>
-              <p class="mb-3">
-                <a class="btn btn-success" href="<?php echo esc_url( $wa ); ?>" target="_blank" rel="noopener"><i class="fa fa-whatsapp pe-2"></i>Order</a>
-              </p>
-            <?php endif; ?>
+            <div class="d-flex justify-content-start align-items-center">
+              <?php
+                $live = get_post_meta( get_the_ID(), '_wssp_url_live_preview', true );
+                $wa = function_exists('wssp_get_whatsapp_order_url') ? wssp_get_whatsapp_order_url( get_the_ID() ) : '';
+                if ( $live ) :
+              ?>
+                <p class="my-2 mx-1">
+                  <a class="btn btn-primary" href="<?php echo esc_url( $live ); ?>" target="_blank" rel="noopener"><i class="fa fa-eye pe-2"></i>Live Preview</a>
+                </p>
+              <?php endif; ?>
+              <?php if ( ! empty( $wa ) ) : ?>
+                <p class="my-2 mx-1">
+                  <a class="btn btn-success" href="<?php echo esc_url( $wa ); ?>" target="_blank" rel="noopener"><i class="fa fa-whatsapp pe-2"></i>Order</a>
+                </p>
+              <?php endif; ?>
+            </div>
           </div>
           <?php
             $terms = get_the_terms( get_the_ID(), 'jenis-web' );
